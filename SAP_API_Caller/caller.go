832aedbf6f0525b1971359096bfe913c2fe5c25f
Yurino Oga <yurino.o@latona.io>
2022-07-28 12:52:45 +0900

    release

---
+++ SAP_API_Caller/caller.go
@@ -0,0 +1,87 @@
+package sap_api_caller
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	sap_api_output_formatter "sap-api-integrations-social-media-user-profile-reads/SAP_API_Output_Formatter"
+	"strings"
+	"sync"
+
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
+	"golang.org/x/xerrors"
+)
+
+type SAPAPICaller struct {
+	baseURL string
+	apiKey  string
+	log     *logger.Logger
+}
+
+func NewSAPAPICaller(baseUrl string, l *logger.Logger) *SAPAPICaller {
+	return &SAPAPICaller{
+		baseURL: baseUrl,
+		apiKey:  GetApiKey(),
+		log:     l,
+	}
+}
+
+func (c *SAPAPICaller) AsyncGetSocialMediaUserProfile(iD string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "SocialMediaUserProfileCollection":
+			func() {
+				c.SocialMediaUserProfileCollection(iD)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+
+func (c *SAPAPICaller) SocialMediaUserProfileCollection(iD string) {
+	data, err := c.callSocialMediaUserProfileSrvAPIRequirementSocialMediaUserProfileCollection("SocialMediaUserProfileCollection", iD)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+
+}
+
+func (c *SAPAPICaller) callSocialMediaUserProfileSrvAPIRequirementSocialMediaUserProfileCollection(api, iD string) ([]sap_api_output_formatter.SocialMediaUserProfileCollection, error) {
+	url := strings.Join([]string{c.baseURL, "c4codataapi", api}, "/")
+	req, _ := http.NewRequest("GET", url, nil)
+
+	c.setHeaderAPIKeyAccept(req)
+	c.getQueryWithSocialMediaUserProfileCollection(req, iD)
+
+	resp, err := new(http.Client).Do(req)
+	if err != nil {
+		return nil, xerrors.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToSocialMediaUserProfileCollection(byteArray, c.log)
+	if err != nil {
+		return nil, xerrors.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) setHeaderAPIKeyAccept(req *http.Request) {
+	req.Header.Set("APIKey", c.apiKey)
+	req.Header.Set("Accept", "application/json")
+}
+
+func (c *SAPAPICaller) getQueryWithSocialMediaUserProfileCollection(req *http.Request, iD string) {
+	params := req.URL.Query()
+	params.Add("$filter", fmt.Sprintf("ID eq '%s'", iD))
+	req.URL.RawQuery = params.Encode()
+}
